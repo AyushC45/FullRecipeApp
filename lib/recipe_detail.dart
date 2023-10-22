f87c3f9d8f09790aeb6280197a809cc9e4eff392
AyushC45 <ayush.chokshi2001@gmail.com>
2023-10-22 16:35:44 -0400

    Cleaned up recipe page: Added bullet points for ingredients and fixed spacing and alignment

--- lib/recipe_detail.dart
+++ lib/recipe_detail.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:localstorage/localstorage.dart';
 
 class RecipeDetail extends StatefulWidget {
   final int recipeId;
@@ -16,6 +17,7 @@ class _RecipeDetailState extends State<RecipeDetail> {
   String recipeName = "";
   List<dynamic> ingredients = [];
   String instructions = "";
+  final localStorage = LocalStorage('recipe_data.json');
 
   @override
   void initState() {
@@ -26,8 +28,7 @@ class _RecipeDetailState extends State<RecipeDetail> {
   Future<void> fetchRecipeDetails() async {
     final apiKey = 'ba0cf77242msh348aa0ac45fded0p1de99cjsnbc922a1ac5d6';
     final url = Uri.parse(
-        'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${widget
-            .recipeId}/information');
+        'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${widget.recipeId}/information');
 
     try {
       final response = await http.get(url, headers: {
@@ -62,49 +63,94 @@ class _RecipeDetailState extends State<RecipeDetail> {
     }
   }
 
+  // Function to save the recipe data to local storage
+  void saveRecipeToLocalStorage() {
+    final recipeData = {
+      'recipeId': widget.recipeId,
+      'recipeName': recipeName,
+      'imageUrl': widget.imageUrl,
+    };
+
+    List<dynamic> starRecipes = localStorage.getItem("starRecipes") ?? [];
+    starRecipes.add(recipeData); // Add the new recipe data to the list
+
+    localStorage.setItem('starRecipes', starRecipes);
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(recipeName),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            if (widget.imageUrl.isNotEmpty) Image.network(widget.imageUrl),
-            // Display the recipe image if URL is not empty
-            SizedBox(height: 16.0),
-            ElevatedButton.icon(
-              onPressed: () {
-                // Add your function here if needed
-              },
-              icon: Icon(Icons.calendar_today),
-              label: Text('Add to Meal Plan'),
-            ),
-
-            SizedBox(height: 16.0),
-            Text(
-              'Ingredients:',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            Text(ingredients.join('\n')),
-            SizedBox(height: 16.0),
-            Text(
-              'Instructions:',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 16.0),
-              child: Center(
-                child: Text(
-                  instructions,
-                  style: TextStyle(fontSize: 16),
-                  textAlign: TextAlign.center,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.network(
+                widget.imageUrl,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton.icon(
+                onPressed: saveRecipeToLocalStorage,
+                icon: Icon(Icons.calendar_today),
+                label: Text('Add to Meal Plan'),
+              ),
+              SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 16.0), // Add left margin
+                    child: Text(
+                      'Ingredients:',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: ingredients.map((ingredient) {
+                  return Text(
+                    '• $ingredient',
+                    style: TextStyle(fontSize: 16),
+                  );
+                }).toList(),
+              ),
+              SizedBox(height: 30.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 16.0), // Add left margin
+                    child: Text(
+                      'Instructions:',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10.0),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      instructions,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 25.0),
+                  ],
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
